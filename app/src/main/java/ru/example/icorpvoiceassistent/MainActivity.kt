a3e7986ad04a60a515436e7a35f67a57a3b87c6a
ZiganshinIB <elzig3012@gmail.com>
2023-01-31 23:14:26 +0300

    feat: Add Log

--- app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
+++ app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
@@ -2,9 +2,11 @@ package ru.example.icorpvoiceassistent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +27,8 @@ class MainActivity : AppCompatActivity() {
 
         val output: TextView = findViewById(R.id.output)
         output.text = summary
+
+        Log.d(TAG, summary)
 
 
     }
