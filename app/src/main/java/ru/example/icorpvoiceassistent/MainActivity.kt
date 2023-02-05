fa95e40dbafc3eb7372ddc08cfd8edeb96847f96
ZiganshinIB <elzig3012@gmail.com>
2023-02-05 14:47:59 +0300

    add toolbar_menu

--- app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
+++ app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
@@ -1,8 +1,11 @@
 package ru.example.icorpvoiceassistent
 
+import android.content.ClipData.Item
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -18,5 +21,24 @@ class MainActivity : AppCompatActivity() {
     fun initViews(){
         val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_stop -> {
+                Log.d(TAG, "action_stop")
+                return true
+            }
+            R.id.action_clear -> {
+                Log.d(TAG, "action_clear")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
