7b060c905a041fd84d3b926bfa6dca3a0e859928
ZiganshinIB <elzig3012@gmail.com>
2023-02-06 21:47:38 +0300

    Add PodAdapter

--- app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
+++ app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
@@ -6,11 +6,35 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Adapter
+import android.widget.SimpleAdapter
 import android.widget.TextView
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
+    lateinit var requestInput: TextInputEditText
+    lateinit var podsAdapter: SimpleAdapter
+
+    val pods = mutableListOf<HashMap<String, String>>(
+        HashMap<String,String>().apply {
+            put("Title", "Title 1")
+            put("Content", "Content 1")
+        },
+        HashMap<String,String>().apply {
+            put("Title", "Title 2")
+            put("Content", "Content 2")
+        },
+        HashMap<String,String>().apply {
+            put("Title", "Title 3")
+            put("Content", "Content 3")
+        },
+        HashMap<String,String>().apply {
+            put("Title", "Title 4")
+            put("Content", "Content 4")
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
