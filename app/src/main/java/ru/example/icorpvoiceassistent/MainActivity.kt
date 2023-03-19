af0a0c8760e2b37ec744d4191ca74010ca7f5ddc
ZiganshinIB <ZiganshinIB@nknh.sibur.ru>
2023-03-19 18:43:36 +0300

    Less 4

--- app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
+++ app/src/main/java/ru/example/icorpvoiceassistent/MainActivity.kt
@@ -1,10 +1,13 @@
 package ru.example.icorpvoiceassistent
 
 import android.content.ClipData.Item
+import android.content.Intent
 import android.media.tv.AdRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.LocaleList
 import android.os.Message
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.Display
@@ -27,6 +30,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.*
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "MainActivity"
@@ -40,6 +45,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var textToSpeech: TextToSpeech
 
     var isTtsReady: Boolean = false
+
+    val VOICE_RECOGNITION_REQUEST_CODE: Int = 777
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -86,7 +93,12 @@ class MainActivity : AppCompatActivity() {
 
         val voiceInputButton: FloatingActionButton = findViewById(R.id.voice_input_button)
         voiceInputButton.setOnClickListener {
-            Log.d(TAG, "FAB")
+            pods.clear()
+            podsAdapter.notifyDataSetChanged()
+            if (isTtsReady){
+                textToSpeech.stop()
+            }
+            showVoiceInputDialog()
         }
 
         progressBar = findViewById(R.id.progress_bar)
@@ -185,5 +197,29 @@ class MainActivity : AppCompatActivity() {
             }
         }
         textToSpeech.language = Locale.US
+    }
+
+    fun showVoiceInputDialog(){
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply{
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.request_hint))
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+        }
+        runCatching {
+            startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE )
+        }.onFailure { t ->
+
+            showSnackbar(t.message ?: getString(R.string.error_voice_recognition_unavailable))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK){
+            data?.getStringArrayExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0)?.let{ question ->
+                requestInput.setText(question)
+                askWolfram(question)
+            }
+        }
     }
 }
